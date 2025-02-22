674c68e4ccd159b2ebdec1a5ac3efad64997d2fc
goferwplynie <dawidmarkiewicz214@gmail.com>
2025-02-22 11:20:29 +0100

    >:3c

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+type Config struct {
+	Cursor                string
+	MaxFilesToLoadAtStart int
+}
+
+func New() *Config{ 
+	return &Config{
+		Cursor: ">",
+		MaxFilesToLoadAtStart: 100,
+	}
+}
--- internal/filepicker/filepicker.go
+++ internal/filepicker/filepicker.go
@@ -2,8 +2,10 @@ package filepicker
 
 import (
 	"errors"
+	"log"
 	"os"
 
+	"github.com/goferwplynie/goXP/config"
 	"github.com/goferwplynie/goXP/internal/models"
 )
 
@@ -15,10 +17,33 @@ type Model struct {
 	Selected          int
 }
 
+var conf= config.New()
+
 func (m *Model) FindFiles() error {
 	files, err := os.ReadDir(m.CurrentPath)
 
 	if err != nil {
 		return errors.New("Unable to read this directory")
 	}
+
+	if len(files) > conf.MaxFilesToLoadAtStart{
+		for i:=0;i<=conf.MaxFilesToLoadAtStart;i++{
+			file := files[i]
+			info, err := file.Info()
+			if err != nil{
+				log.Printf("%v: %v", file.Name(), err)
+			}
+			fileObj := models.File{
+				Name: info.Name(),
+				Size: info.Size(),
+				Mode: uint32(info.Mode()),
+				ModTime: info.ModTime(),
+				IsDir: info.IsDir(),
+			}
+
+			m.Files = append(m.Files, fileObj)
+		}
+	}
+
+	return nil
 }
